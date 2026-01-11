1a76116204aea7a470a77b5b70a65df24c115f2a
username-b <60197242+username-b@users.noreply.github.com>
2026-01-11 20:51:46 +0300

    aggTrades

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ SYMBOL = "ADAUSDT"
 YC_BUCKET = "binance-data-downloader"
 DAYS_BACK = 1800
 INTERVAL = "1m"
-DATASET = "klines"
+DATASET = "aggTrades"
 # возможные значения:
 # klines | aggTrades | trades | bookTicker
 
 BASE_ROOT = "https://data.binance.vision/data/futures/um/daily"
-SOURCE = "klines"
+SOURCE = "aggTrades"
 
 DATA_ROOT = r"C:\projects\binance-data-downloader\data\raw"
 
